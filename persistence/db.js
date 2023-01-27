74897f5e9007bb60154ca8e0172c05b6e18d5405
htlWels <franz.reitinger@htl-wels.at>
2023-01-27 09:23:06 +0100

    mongo

--- persistence/db.js
+++ persistence/db.js
@@ -59,21 +59,21 @@ process.on('SIGINT', function() {
 const mongoose = require('mongoose');
 
 
-const uri = 'mongodb+srv://horus:47oldEgyptKing47@risingsun-4o7zv.gcp.mongodb.net/ahit5?retryWrites=true';
+const uri = 'mongodb+srv://refr:47rf-winnt75@cluster0.gyig1.mongodb.net/admin?retryWrites=true';
 // uri = 'mongodb://localhost/ahit5?retryWrites=true'
 
 // uri = "mongodb+srv://horus:47oldEgyptKing47@risingsun-4o7zv.gcp.mongodb.net/wawi?retryWrites=true?ssl=true"
 
 const options = {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
     autoIndex: false, // Don't build indexes
-    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
+    //reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    //reconnectInterval: 500, // Reconnect every 500ms
+    //poolSize: 10, // Maintain up to 10 socket connections
     // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0,
+    //bufferMaxEntries: 0,
     connectTimeoutMS: 10000, // Give up initial connection after 10 seconds
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
