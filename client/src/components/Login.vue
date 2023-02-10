e76b1987cceff447139b2ccdb12bf78a441285dd
htlWels <franz.reitinger@htl-wels.at>
2023-02-10 13:04:10 +0100

    login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -1,4 +1,89 @@
 <template>
+  <div class="login-page bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-10 offset-lg-1">
+          <h3 class="mb-3">Login Now</h3>
+          <div class="bg-white shadow rounded">
+            <div class="row">
+              <div class="col-md-7 pe-0">
+                <div class="form-left h-100 py-5 px-5">
+                  <form action="" class="row g-4">
+                    <div class="col-12">
+                      <label>Username<span class="text-danger">*</span></label>
+                      <div class="input-group">
+                        <div class="input-group-text">
+                          <i class="bi bi-person-fill"></i>
+                        </div>
+                        <input
+                          type="text"
+                          v-model="username"
+                          class="form-control"
+                          placeholder="Enter Username"
+                        />
+                      </div>
+                    </div>
+
+                    <div class="col-12">
+                      <label>Password<span class="text-danger">*</span></label>
+                      <div class="input-group">
+                        <div class="input-group-text">
+                          <i class="bi bi-lock-fill"></i>
+                        </div>
+                        <input
+                          type="text"
+                          v-model="password"
+                          class="form-control"
+                          placeholder="Enter Password"
+                        />
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="form-check">
+                        <input
+                          class="form-check-input"
+                          type="checkbox"
+                          id="inlineFormCheck"
+                        />
+                        <label class="form-check-label" for="inlineFormCheck"
+                          >Remember me</label
+                        >
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <a href="#" class="float-end text-primary"
+                        >Forgot Password?</a
+                      >
+                    </div>
+                    <div class="col-12">
+                      <button
+                        @click="login"
+                        type="submit"
+                        class="btn btn-primary px-4 float-end mt-4"
+                      >
+                        login
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+              <div class="col-md-5 ps-0 d-none d-md-block">
+                <div
+                  class="form-right h-100 bg-primary text-white text-center pt-5"
+                >
+                  <i class="bi bi-bootstrap"></i>
+                  <h2 class="fs-1">Welcome Back!!!</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p class="text-end text-secondary mt-3">
+            Bootstrap 5 Login Page Design
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- Bootstrap JS -->
 
@@ -6,14 +91,7 @@
 </template>
 
 <script setup>
-import { ref } from "vue";
-
-let username = ref("");
-let password = ref("");
-
-function login() {
-
-}
+ 
 </script>
 
 <style>
