90f5af7291fa290d23e6c10501647e70c2d0f70d
htlWels <franz.reitinger@htl-wels.at>
2023-03-23 20:25:11 +0100

    client

---
+++ client1/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="dashboard">
+    <Dashboard  />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Dashboard from '@/components/Dashboard.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    Dashboard,
+  },
+};
+</script>
+<style scoped>
+.dashboard {
+  margin-bottom: 50px;
+}
+</style>
